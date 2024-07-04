2769503f49c577aa3648f67c6a70bf480789c899
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-04 12:10:53 -0400

    navbar - dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import logo from "../img/logo.png";
 
@@ -10,12 +18,30 @@ const Navbar = () => {
             <Link href="/">
                 <Image src={logo} alt="CompanyLogo" width={40} />
             </Link>
-            <Avatar>
-                <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                />
-            </Avatar>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger className="focus:outline-none">
+                    <Avatar>
+                        <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                        />
+                        <AvatarFallback className="text-slate-900 bg-rose-400">
+                            K
+                        </AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <Link href="/profile">Profile</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link href="/auth">Logout</Link>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     );
 };
