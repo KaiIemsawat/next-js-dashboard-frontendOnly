b406b9a0b95bd4e915d0618b22b7a5dbc19f6176
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-05 09:22:46 -0400

    dashboard icon - base

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Newspaper } from "lucide-react";
 
 export default function Home() {
     return (
         <>
-            <h1 className="text-2xl">Dashboard</h1>
+            <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+                <DashboardCard
+                    title="Posts"
+                    count={100}
+                    icon={<Newspaper className="text-slate-500" size={72} />}
+                />
+            </div>
         </>
     );
 }
