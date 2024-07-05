f2cc1842a40691d27e7ccc4c33cd6893e2ee0293
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-05 13:42:03 -0400

    post table - 2

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -1,10 +1,11 @@
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCaption,
 } from "@/components/ui/table";
 import Link from "next/link";
 
@@ -12,17 +13,48 @@ import posts from "@/data/posts";
 import { Post } from "@/types/posts";
 
 interface PostsTableProps {
-    limit?: number;
-    title?: string;
+  limit?: number;
+  title?: string;
 }
 
 const PostsTable = ({ limit, title }: PostsTableProps) => {
-    return (
-        <div className="mt-10">
-            <h3 className="text-2xl mb-4 font-semibold">
-                {title ? title : "Posts"}
-            </h3>
-        </div>
-    );
+  return (
+    <div className="mt-10">
+      <h3 className="mb-4 text-2xl font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>A list of recent posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden text-right md:table-cell">
+              Date
+            </TableHead>
+            <TableHead>View</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {posts.map((post) => (
+            <TableRow key={post.id}>
+              <TableCell>{post.title}</TableCell>
+              <TableCell className="hidden md:table-cell">
+                {post.author}
+              </TableCell>
+              <TableCell className="hidden text-right md:table-cell">
+                {post.date}
+              </TableCell>
+              <TableCell>
+                <Link href={`/posts/edit/${post.id}`}>
+                  <button className="rounded bg-blue-500 px-4 py-2 text-xs font-bold text-white hover:bg-blue-700">
+                    Edit
+                  </button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 export default PostsTable;
