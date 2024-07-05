843c23b4502790c2599fe4a85e38aa945acba909
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-05 13:07:09 -0400

    post table - 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
-import { Newspaper } from "lucide-react";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
+
+import PostsTable from "@/components/posts/PostsTable";
 
 export default function Home() {
     return (
@@ -10,7 +12,25 @@ export default function Home() {
                     count={100}
                     icon={<Newspaper className="text-slate-500" size={72} />}
                 />
+                <DashboardCard
+                    title="Categories"
+                    count={12}
+                    icon={<Folder className="text-slate-500" size={72} />}
+                />
+                <DashboardCard
+                    title="Users"
+                    count={750}
+                    icon={<User className="text-slate-500" size={72} />}
+                />
+                <DashboardCard
+                    title="Comments"
+                    count={1200}
+                    icon={
+                        <MessageCircle className="text-slate-500" size={72} />
+                    }
+                />
             </div>
+            <PostsTable title="Latest Posts" />
         </>
     );
 }
---
+++ components/posts/PostsTable.tsx
@@ -0,0 +1,28 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+
+import posts from "@/data/posts";
+import { Post } from "@/types/posts";
+
+interface PostsTableProps {
+    limit?: number;
+    title?: string;
+}
+
+const PostsTable = ({ limit, title }: PostsTableProps) => {
+    return (
+        <div className="mt-10">
+            <h3 className="text-2xl mb-4 font-semibold">
+                {title ? title : "Posts"}
+            </h3>
+        </div>
+    );
+};
+export default PostsTable;
