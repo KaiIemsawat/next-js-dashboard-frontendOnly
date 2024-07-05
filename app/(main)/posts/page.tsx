657a5bd75ce1b60b41dbdce312524e23e62290f9
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-05 17:07:58 -0400

    pagination

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,12 +1,13 @@
 import BackButton from "@/components/BackButton";
-
 import PostsTable from "@/components/posts/PostsTable";
+import PostsPagination from "@/components/posts/PostsPagination";
 
 const page = () => {
   return (
     <>
       <BackButton text="Go Back" link="/" />
       <PostsTable />
+      <PostsPagination />
     </>
   );
 };
